630e9391f28ac0789f3f69d321b2750f6453bef8
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-06-19 16:30:47 +0200

    com 2

--- Es 3/Program.cs
+++ Es 3/Program.cs
@@ -4,17 +4,24 @@
     {
         static void Main(string[] args)
         {
+
+            //do un valore di lunghezza all'array
             int x = 0;
-            Console.WriteLine("Grandezza Array");
+            Console.WriteLine(" decidi la Grandezza dell'array ");
             x = int.Parse(Console.ReadLine());
             int[] lista = new int[x];
             int i;
             int som = 0;
+
+            //do dei valori agli elementi degli array
             for (i = 0; i < lista.Length; i++)
             {
-                Console.WriteLine("inserisci");
+                Console.WriteLine("inserisci valore del singolo elemento dell'array");
                 lista[i] = int.Parse(Console.ReadLine());
             }
+
+
+            //calcolo la somma dei valori degli array
             for (i = 0; i < lista.Length; i++)
             {
                 som += lista[i];
